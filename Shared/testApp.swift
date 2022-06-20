8aad2e5771bc1d420598118a5c91b5fa318e407d
Dmitry Kosik <dmtkosik@gmail.com>
2022-06-20 14:16:28 +0300

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Dmitry Kosik on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
